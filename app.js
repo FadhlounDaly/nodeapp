bb7f141d9385044a531362b25ad8cd7e6159237e
FadhlounDaly <53160481+FadhlounDaly@users.noreply.github.com>
2020-06-23 17:20:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use('/',(req,res)=>{
-    res.send('hello from Docker again');
+    res.send('hello from Jenkins again');
 })
 
 app.listen(8080,()=>{
